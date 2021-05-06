d154adf33cca4fda968da60d0652d4c5301e11d6
orges <me@orgesified.ml>
2021-05-06 00:07:02 +0000

    apple cringe llvm prebuilts update on commit https://github.com/apple/llvm-project/commit/8a09b3139fd4
    Signed-off-by: orges <me@orgesified.ml>

--- include/llvm/Support/VCSRevision.h
+++ include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "213d85d2db7a6730a06bc6707af06850fb3561cb"
+#define LLVM_REVISION "8a09b3139fd405602a205e98932b576dd05328a7"
 #define LLVM_REPOSITORY "https://github.com/apple/llvm-project"
